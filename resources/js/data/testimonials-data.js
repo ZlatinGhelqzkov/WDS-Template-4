8b7bd56328713907cb39b056861fcaea21df81ab
Zlatin Zhelyazkov <zlatinzhelyazkov98@gmail.com>
2024-03-05 13:48:40 +0200

    More URL fixes

--- resources/js/data/testimonials-data.js
+++ resources/js/data/testimonials-data.js
@@ -1,21 +1,21 @@
 var testimonialsData = [
     {
-        img_source: '../../images/testimonials-carousel/beatrice.png',
+        img_source: './resources/images/testimonials-carousel/beatrice.png',
         name: 'Beatrice thierry',
         description: `Nous avons passé un excellent séjour en Bulgarie qui nous a ravit. Nous avons appérécié le choix des hébergements et des lieux à visiter. L'organisation par Balkania Tour a été parfaite.`
     },
     {
-        img_source: '../../images/testimonials-carousel/miguel.png',
+        img_source: './resources/images/testimonials-carousel/miguel.png',
         name: 'Miguel Angel',
         description: `Ha sido todo un acierto, a pesar de las dudas del principio, por no conocer la empresa y ser todo por internet, Me atrajo la forma de presentar el viaje y la variada informacion que te ofrecen. A pesar de lo muchos kilomtros que recorres no ha sido pesado y con el viaje tienes un conocimiento muy amplio del pais.`
     },
     {
-        img_source:'../../images/testimonials-carousel/group.png',
+        img_source:'./resources/images/testimonials-carousel/group.png',
         name: 'jerome,lily,swan,alexandra',
         description: `Nous revenons d'un beau voyage en Bulgarie. Pays facile d'accès et facile à voyager. Par contre, l'expérience humaine n'est pas leur fort. Des villages superbes dans les montagnes, et des paysages magnifiques dans le massif du balkan (col de shipka par exemple.) un pays encore traditionnel mais de façon moins prononcé que chez son voisin Roumain.`
     },
     {
-        img_source: '../../images/testimonials-carousel/etavard.png',
+        img_source: './resources/images/testimonials-carousel/etavard.png',
         name: 'etavard',
         description: `Partir à la découverte de ce pays fut facile grâce aux organisateurs. Tout était trés clair, avec les adresses précises des hôtels (adresses rentrées dans les favoris du GPS de location), les sites à visiter.  En partant de la capitale vers les montagnes, avec un détour par la côte de la Mer Noire, puis en rentrant par les Balkans. Nous avons eu une semaine riche en découverte, avec ses coutumes & tradition!`
     },
